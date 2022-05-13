d1705a7e42d2eddb65410ff9bc4b61dcfa697db6
JoannaBusmail <joanna.busmail@gmail.com>
2022-05-13 10:30:14 +0200

    add filter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const Allmovies = require('./movies.json');
+//guardo el json de mi listado de peliculas
+const allMovies = require('./movies.json');
 
 // create and config server
 const server = express();
@@ -12,11 +13,15 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-
+//pido al servidor a través de un objeto ese listado de peliculas que esta guardada en esa constante
 server.get('/movies', (req, res) => {
+  const genderFilterParams = req.query.gender ? req.query.gender : '';
   console.log('Vamos a preparar un JSON');
+
   res.json({
     success: true,
-    movies: Allmovies,
+    movies: allMovies.filter((eachMovie) =>
+      eachMovie.gender.includes(genderFilterParams)
+    ),
   });
 });
