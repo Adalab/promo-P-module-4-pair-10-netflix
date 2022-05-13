a1249ee6de235f0fc19a5afcd791fc22945fc4cb
JoannaBusmail <joanna.busmail@gmail.com>
2022-05-13 10:50:44 +0200

    add filter gender

--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,7 +1,7 @@
 // login
 //le paso value como parametro que viene de all movies donde estan los select de los filtros
 const getMoviesFromApi = (value) => {
-  console.loh(getMoviesFromApi);
+  console.log(getMoviesFromApi);
   console.log('Se están pidiendo las películas de la app');
   //Incluyo la nueva ruta donde pediremos al servidor
   //le paso params gender
